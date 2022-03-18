36217293f27976f859941631cbe7cee2905864ff
Simodalstix <lastpolar@gmail.com>
2022-03-18 11:54:58 +1100

    Testing module.exports

--- Udemy/catShelter/index.js
+++ Udemy/catShelter/index.js
@@ -4,5 +4,5 @@ const allCats = [star, storm]
 let simon = "lets test the rainbow"
 
 const rainbow = require('colors');
-console.log(simon.zebra);
+console.log(simon.rainbow);
 
---
+++ app.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+console.log(math);
+
+console.log(math.PI);
+console.log(math.square(20));
+console.log(math.add(235, 1223));
